f3457a5d79fe2421000cc3c2e2c39d5468ed2452
Tanu Chauhan <tanuchauhan16092002@gmail.com>
2023-12-20 11:41:06 +0530

    commit

---
+++ twit_count.js
@@ -0,0 +1,5 @@
+var twit = prompt("Compose your tweet");
+var twitlength = twit.length;
+alert("You have written" + twitlength + " characters , you have" + (200-twitlength) + " characters remaining");
+
+
