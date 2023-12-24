e4e4a0f0eaf2d3cabab27761c7ccede414a7e5f8
Tanu Chauhan <tanuchauhan16092002@gmail.com>
2023-12-24 15:36:58 +0530

    commit

--- string_casing_challenge.js
+++
@@ -1,7 +0,0 @@
-
-var name = prompt("What is your name");
-var s = name.slice(0,1);
-var uppercase_s = s.toUpperCase();
-var restofname = name.slice(1,name.length);
-var final_Name = uppercase_s + restofname;
-alert("Hello " + final_Name);
