3ba9b6306a99988a584b8529544bca3daa0e8c6a
Tanu Chauhan <tanuchauhan16092002@gmail.com>
2023-12-29 12:08:46 +0530

    commit

---
+++ Array_Challenge.js
@@ -0,0 +1,21 @@
+var output = [];
+var count = 1;
+
+function fizzbuzz() {
+    if(count%3 ===0 && count%5 ===0){
+        output.push("fizzbuzz");
+    }
+    else if(count % 5 === 0){
+    output.push("Buzz");
+    }
+    else if(count%3 === 0){
+    output.push("fizz");
+    }
+    else{
+output.push (count);
+    }   
+    count++;
+
+ console.log (output);
+
+}
